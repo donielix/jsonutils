8a6c1174e69131e4cb7744a02d06ac797e115d13
Daniel Diego Horcajuelo <dadiego91@gmail.com>
2021-07-04 14:47:08 +0200

    fix build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker stop json-queries && \
-docker rm json-queries && \
+docker stop json-queries || true && \
+docker rm json-queries || true && \
 docker image prune -f && \
 docker build -t json-queries . && \
 docker run --name json-queries -it json-queries
