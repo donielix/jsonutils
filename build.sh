b0e21f3d2eb4f1c62fccca29e74eeada4f66614b
Daniel Diego Horcajuelo <dadiego91@gmail.com>
2021-07-22 12:31:43 +0200

    add optional volume in build script

--- build.sh
+++ build.sh
@@ -4,4 +4,13 @@ docker stop json-queries || true && \
 docker rm json-queries || true && \
 docker image prune -f && \
 docker build -t json-queries . && \
-docker run --rm --name json-queries -it json-queries
+if [[ $# -eq 0 ]]
+then
+    echo "==== Building without volume ===="
+    docker run --rm --name json-queries -it json-queries
+else
+    echo "==== Building with volume ===="
+    echo $1
+    docker run --rm --name json-queries -v $1:/mnt/$1 -it json-queries
+fi
+
