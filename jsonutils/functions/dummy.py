150dd70d3cca5340c3194d97d4bd1c36ddb195c6
Daniel Diego Horcajuelo <dadiego91@gmail.com>
2021-08-27 18:05:41 +0200

    add max_depth parameter to generate json

--- jsonutils/functions/dummy.py
+++ jsonutils/functions/dummy.py
@@ -19,6 +19,7 @@ def dummy_json(
     max_list_length=5,
     min_str_length=10,
     max_str_length=100,
+    max_depth=10,
 ):
     """
     Generate a dummy JSONObject instance
@@ -55,10 +56,8 @@ def dummy_json(
 
     NODE_LIST_SINGLETON = (JSONStr, JSONFloat, JSONInt, JSONBool, JSONNull)
 
-    COMPOSE_OBJECTS = []
-
     def get_random_string(N=4):
-        return "".join(random.choices(string.ascii_lowercase + string.digits, k=N))
+        return "".join(random.choices(string.ascii_lowercase, k=N))
 
     def get_random_node(kind="all"):
         if kind == "all":
@@ -76,39 +75,49 @@ def dummy_json(
         "node must be a JSONSingleton instance"
 
         if node == JSONStr:
-            return JSONStr("AAA")
+            out = get_random_string(8)
+            return JSONStr(out)
         elif node == JSONBool:
-            return JSONBool(True)
+            out = random.choice((True, False))
+            return JSONBool(out)
         elif node == JSONInt:
+            out = random.randint(0, 10000)
             return JSONInt(1)
         elif node == JSONFloat:
-            return JSONFloat(3.8)
+            out = random.random()*100
+            return JSONFloat(out)
         elif node == JSONNull:
             return JSONNull(None)
         else:
             raise TypeError(f"node argument is not a JSONSingleton instance: {node}")
 
-    def fill_composed_node(node):
+    def fill_composed_node(node, it=0):
         "node must be a JSONCompose instance"
 
         if isinstance(node, dict):
             length = random.randint(min_dict_length, max_dict_length)
             for _ in range(length):
                 key = get_random_string()
-                value = get_random_node()  # a node object
+                if it < max_depth:
+                    value = get_random_node(kind="all")  # a node object
+                else:
+                    value = get_random_node(kind="singleton")
                 if value.is_composed:
-                    value = value()  # initialize composed object
-                    COMPOSE_OBJECTS.append(value)
+                    value = value()
+                    fill_composed_node(value, it + 1)
                 else:
                     value = initialize_singleton(value)
                 node.__setitem__(key, value)
         elif isinstance(node, list):
             length = random.randint(min_list_length, max_list_length)
             for _ in range(length):
-                item = get_random_node()
+                if it < max_depth:
+                    item = get_random_node(kind="all")
+                else:
+                    item = get_random_node(kind="singleton")
                 if item.is_composed:
                     item = item()
-                    COMPOSE_OBJECTS.append(item)
+                    fill_composed_node(item, it + 1)
                 else:
                     item = initialize_singleton(item)
                 node.append(item)
